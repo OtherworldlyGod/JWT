899b7825e87f89971e4ce7bcd06dab256f9b606f
OtherworldlyGod <vedantthedevil.com>
2025-12-28 21:45:55 +0530

    add basic login endpoint issuing JWT (no auth yet)

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken')
 
-app.listen(3000)
+app.use(express.json())
+
+
 
 const posts =[
     {
@@ -14,7 +19,18 @@ const posts =[
         title: 'Post 2'
     }
 ]
+
 app.get('/posts',(req,res) =>{
     res.json(posts)
 }
 )
+app.post('/login',(req,res) =>{
+    //Authenticate the user
+
+    const username = req.body.username
+    const user = {name:username}
+    const accesstoken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accesstoken:accesstoken})
+})
+
+app.listen(3000)
