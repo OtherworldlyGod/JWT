b29eec2f9f9ae63904ec0bb8dd61d41e529717e8
OtherworldlyGod <vedantthedevil.com>
2025-12-28 20:48:21 +0530

    add REST client request example

--- server.js
+++ server.js
@@ -3,3 +3,18 @@ const app = express()
 
 app.listen(3000)
 
+const posts =[
+    {
+        username: 'Vedant',
+        title: 'Post 1'
+    }
+    ,
+    {
+        username: 'Wade',
+        title: 'Post 2'
+    }
+]
+app.get('/posts',(req,res) =>{
+    res.json(posts)
+}
+)
