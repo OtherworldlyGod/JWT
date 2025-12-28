cfb754fed86beb11e4ddaab192eb2ac01714179d
OtherworldlyGod <vedantthedevil.com>
2025-12-28 22:11:31 +0530

    dd JWT token issuance and auth middleware

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ const posts =[
     }
 ]
 
-app.get('/posts',(req,res) =>{
-    res.json(posts)
+app.get('/posts',authenticatetoken,(req,res) =>{
+
+    res.json(posts.filter(post => post.username === req.user.name))
 }
 )
 app.post('/login',(req,res) =>{
@@ -32,5 +33,17 @@ app.post('/login',(req,res) =>{
     const accesstoken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
     res.json({accesstoken:accesstoken})
 })
+
+function authenticatetoken(req,res,next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return res.sendStatus(401)
+        
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user) =>{
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+})
+}
 
 app.listen(3000)
